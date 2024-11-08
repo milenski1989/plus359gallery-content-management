7a6dbde446f43cb86df7f99e76c01a8bf066c505
milenski1989 <milenski1989@gmail.com>
2024-11-08 20:01:16 +0200

    update env vars usage based on prod or dev envs

--- server/src/s3Client/s3Client.js
+++ server/src/s3Client/s3Client.js
@@ -3,7 +3,7 @@ import AWS from 'aws-sdk';
 
 const s3Client = new AWS.S3({
     endpoint: "nyc3.digitaloceanspaces.com",
-    region: 'nyc3',
+    region: 'us-east-1',
     credentials: {
       accessKeyId: process.env.SPACES_KEY,
       secretAccessKey: process.env.SPACES_SECRET,
